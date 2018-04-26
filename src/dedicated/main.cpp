76a6149864658c9551b4659fecd14f5087dc4df2
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-04-26 14:25:50 +0000

    Add dedicated server launcher [ci skip]

---
+++ src/dedicated/main.cpp
@@ -0,0 +1,34 @@
+#include <cstdlib>
+#include "shiftutil/shared_lib.hpp"
+#include "vengine/IEngineCore.hpp"
+
+int main(int argc, char **argv)
+{
+	shiftutil::shared_lib EngineCoreLib("VEngineCore");
+	
+	//if(!EngineCoreLib) // TODO: add operator bool overload to shared_lib
+		//return EXIT_FAILURE;
+	
+	auto fnGetEngineCore{EngineCoreLib.getexportfunc<pfnGetEngineCore>("GetEngineCore")};
+	
+	if(!fnGetEngineCore)
+		return EXIT_FAILURE;
+	
+	auto pEngine{fnGetEngineCore(IEngineCore::Version)};
+	
+	if(!pEngine)
+		return EXIT_FAILURE;
+	
+	IEngineCore::InitParams VEngineInitParams{};
+	
+	VEngineInitParams.ExecMode = IEngineCore::Mode::DedicatedServer;
+	
+	pEngine->Init(VEngineInitParams);
+	
+	while(pEngine->Frame()) // TODO: geez...
+		;
+	
+	pEngine->Shutdown();
+	
+	return EXIT_SUCCESS;
+};
