fe2267c145dcc3c4bf4513983490e6156bbe2711
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-04-27 13:18:06 +0000

    WIP network module implementation

---
+++ src/engine/network/Exports.cpp
@@ -0,0 +1,13 @@
+/// @file
+
+#include "vengine/CommonTypes.hpp"
+#include "Network.hpp"
+
+C_EXPORT INetwork *GetNetwork(int version)
+{
+	if(version == INetwork::Version)
+	{
+		static CNetwork Network;
+		return &Network;
+	};
+};
