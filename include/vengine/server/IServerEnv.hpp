0f0589eeb317f831d5a00459ce93df5e88b1117a
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-04-29 11:59:30 +0000

    Add server-side engine environment interface [ci skip]

---
+++ include/vengine/server/IServerEnv.hpp
@@ -0,0 +1,19 @@
+/// @file
+/// @brief server-side engine environment interface - contains a set of interfaces accessible to use by the server-side game
+
+#pragma once
+
+struct ICmdRegistry;
+struct IPhysics;
+
+struct IEngineEnv
+{
+	///
+	virtual ICmdRegistry *GetCmdRegistry() const = 0;
+	
+	///
+	virtual ICvarRegistry *GetCvarRegistry() const = 0;
+	
+	///
+	virtual IPhysics *GetPhysics() const = 0;
+};
