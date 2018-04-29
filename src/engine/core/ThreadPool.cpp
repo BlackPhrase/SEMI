3cae8ccc543ebb9a793dee37f8d7c8a31693cbd1
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-04-29 11:54:11 +0000

    Update thread pool implementation [ci skip]

--- src/engine/core/ThreadPool.cpp
+++ src/engine/core/ThreadPool.cpp
@@ -1,12 +1,31 @@
 /// @file
 
+#include <cstdio>
+#include <thread>
+#include <atomic>
+#include <mutex>
+
 #include "ThreadPool.hpp"
+
+std::mutex mutex;
 
 void ThreadLoop()
 {
+	using namespace std::chrono_literals;
+	
+	static std::atomic_int nLoop;
+	
 	while(true)
 	{
 		// TODO: do something
+		
+		if(nLoop != 100)
+		{
+			std::this_thread::sleep_for(1s);
+			std::lock_guard<std::mutex> guard(mutex);
+			printf("ThreadLoop (id #%d)\n", std::this_thread::get_id());
+			++nLoop;
+		};
 	};
 };
 
@@ -21,8 +40,11 @@ CThreadPool::CThreadPool(int anThreadCount)
 	for(auto i = 0; i < anThreadCount; ++i)
 	{
 		mvThreads.emplace_back(ThreadLoop);
-		//mvThreads[i].detach();
+		mvThreads[i].detach();
 	};
+	
+	printf("ThreadPool: hardware_concurrency is %d\n", std::thread::hardware_concurrency());
+	printf("ThreadPool: %d worker thread(s) spawned\n", anThreadCount);
 };
 
 CThreadPool::~CThreadPool() = default;
