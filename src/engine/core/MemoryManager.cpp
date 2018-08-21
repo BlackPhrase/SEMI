99e1926372ee42dc24536fe5538f439c738966e4
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-08-21 12:13:22 +0000

    Update (fix) memory manager implementation

--- src/engine/core/MemoryManager.cpp
+++ src/engine/core/MemoryManager.cpp
@@ -9,14 +9,30 @@ CMemoryManager::~CMemoryManager() = default;
 
 void *CMemoryManager::Alloc(size_t anSize)
 {
-	return malloc(anSize);
+	mnTotalAllocated += anSize;
+	void *pData = malloc(anSize);
+	//mvAllocChunks.push_back(pData);
+	return pData;
 };
 
 void CMemoryManager::Free(void *apData)
 {
-	if(apData)
-	{
-		free(apData);
-		apData = nullptr;
-	};
+	if(!apData)
+		return;
+	
+	//for(auto It : mvAllocChunks)
+		//if(It->mpData == apData)
+		{
+			//mnTotalReleased += It->mnSize;
+			free(apData); //free(It->mpData);
+			apData = nullptr; //It->mpData = apData = nullptr;
+		};
 };
+
+/*
+const SMemStats &CMemoryManager::GetStats() const
+{
+	mStats.mnLeaked = mStats.mnTotalAllocated - mStats.mnTotalReleased;
+	return mStats;
+};
+*/
--- src/engine/core/MemoryManager.hpp
+++ src/engine/core/MemoryManager.hpp
@@ -13,4 +13,7 @@ public:
 	void *Alloc(size_t anSize);
 	void Free(void *apData);
 private:
+	size_t mnTotalAllocated{0}; ///< Total amount of allocated memory
+	size_t mnTotalReleased{0}; ///< Total amount of released memory
+	size_t mnLeaked{0}; ///< Amount of leaked memory (= mnTotalAllocated - mnTotalReleased)
 };
