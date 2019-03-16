36faf433f622f0c32c9a2e6918025209b72aa0ec
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-03-16 13:48:10 +0000

    Update net client interface and move it to network module space

--- include/vengine/network/INetClient.hpp
+++ include/vengine/network/INetClient.hpp
@@ -5,7 +5,6 @@
 
 struct INetMsg;
 struct INetChannel;
-//struct INetAdr;
 
 struct INetClient
 {
@@ -19,14 +18,20 @@ struct INetClient
 	/// @return true if the client is connected to some server
 	virtual bool IsConnected() const = 0;
 	
+	///
+	virtual bool SendConnectionless(const char *asAdr, int anPort, const char *asMsg) = 0;
+	
+	///
+	virtual void SendSequencedPacket(const char *asAdr, int anPort, int anSeqNum, int anServerLastAck, const void *apData) = 0;
+	
+	///
+	virtual void ReceiveSequencedPacket(const char *asAdr, int anPort, int *anSeqNum, int *anClientLastAck, void *apData) = 0;
+	
 	/// Send a message to a server if connected
-	virtual void SendMsg(INetMsg *apMsg) = 0;
+	//virtual void SendMsg(INetMsg *apMsg) = 0;
 	
 	///
-	virtual INetChannel *GetNetChannel() const = 0;
-	
-	///
-	//virtual INetAdr *GetNetAdr() const = 0;
+	//virtual INetChannel *GetNetChannel() const = 0;
 	
 	///
 	//virtual IServerInfo *GetServerInfo() const = 0;
