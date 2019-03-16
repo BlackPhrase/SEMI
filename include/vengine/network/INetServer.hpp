8d70dfd159f0a18bebe1f1f0176dc63a4a33e582
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-03-16 13:45:51 +0000

    Update net server interface and move it to network module space

--- include/vengine/network/INetServer.hpp
+++ include/vengine/network/INetServer.hpp
@@ -6,22 +6,35 @@
 struct IClient;
 struct INetMsg;
 struct INetChannel;
+struct IConnectionlessMsgHandler;
 
 struct INetServer
 {
 	/// @return pointer to a client
-	virtual IClient *GetClient(int id) const = 0;
+	//virtual IClient *GetClient(int id) const = 0;
 	//virtual INetClient *GetClient(int id) const = 0;
 	
 	/// Broadcast the message to each connected client
 	virtual void BroadcastMsg(INetMsg *apMsg) = 0;
 	
+	///
+	virtual bool SendConnectionless(const char *asAdr, int anPort, const char *asMsg) = 0;
+	
+	///
+	virtual void SendSequencedPacket(const char *asAdr, int anPort, int anSeqNum, int anServerLastAck, const void *apData) = 0;
+	
+	///
+	virtual void ReceiveSequencedPacket(const char *asAdr, int anPort, int *anSeqNum, int *anClientLastAck, void *apData) = 0;
+	
 	/// @return pointer to the network channel
-	virtual INetChannel *GetNetChannel() const = 0;
+	//virtual INetChannel *GetNetChannel() const = 0;
 	
 	///
 	virtual int GetClientCount() const = 0;
 	
 	///
 	virtual int GetMaxClients() const = 0;
+	
+	///
+	virtual void AddConnectionlessMsgHandler(IConnectionlessMsgHandler *apHandler) = 0;
 };
