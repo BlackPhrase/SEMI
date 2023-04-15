abb02e48bdc632211de3c611b95f6142ec17165f
BlackPhrase <BlackPhrase@users.noreply.github.com>
2023-04-15 10:35:33 +0000

    Partially add missing license headers

--- include/vengine/core/IFile.hpp
+++ include/vengine/core/IFile.hpp
@@ -1,3 +1,13 @@
+/*
+ * This file is part of V-Engine
+ *
+ * Copyright 2018, 2023 BlackPhrase
+ *
+ * Licensed under terms of the MIT license
+ * See LICENSE.md file for full terms
+ * DO NOT REMOVE THIS NOTICE!
+*/
+
 /// @file
 /// @brief file handle interface
 
