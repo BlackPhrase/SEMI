25fac927b74507c8107b972119eb5d464e8aeaf2
BlackPhrase <BlackPhrase@users.noreply.github.com>
2023-04-15 13:35:10 +0000

    Try to implement the scripting module as an engine module

---
+++ src/engine/script/ScriptModule.cpp
@@ -0,0 +1,62 @@
+/*
+ * This file is part of V-Engine
+ *
+ * Copyright 2018-2019, 2023 BlackPhrase
+ *
+ * Licensed under terms of the MIT license
+ * See LICENSE.md file for full terms
+ * DO NOT REMOVE THIS NOTICE!
+*/
+
+/// @file
+
+#include <vengine/CommonTypes.hpp>
+#include <vengine/IEngineModule.hpp>
+
+#include "Script.hpp"
+
+class CScriptModule : public IEngineModule
+{
+public:
+	CScriptModule(const ICoreEnv &aCoreEnv) : mCoreEnv(aCoreEnv){}
+	~CScriptModule() = default;
+	
+	bool OnLoad() override
+	{
+		return true;
+	};
+	
+	bool OnUnload() override
+	{
+		if(mpScript)
+		{
+			delete mpScript;
+			mpScript = nullptr;
+		};
+		
+		return true;
+	};
+	
+	void *GetInterface(const char *asName, int anVersion) const override
+	{
+		if(!strcmp(asName, "VEngineScript"))
+			if(anVersion == IScript::Version)
+			{
+				if(!mpScript)
+					mpScript = new mpScript(mCoreEnv);
+				return mpScript;
+			};
+		
+		return nullptr;
+	};
+private:
+	const ICoreEnv &mCoreEnv;
+	
+	IScript *mpScript{nullptr};
+};
+
+C_EXPORT IEngineModule *VEngineModuleMain(const ICoreEnv &aCoreEnv)
+{
+	static CScriptModule ScriptEngineModule(aCoreEnv);
+	return &ScriptEngineModule;
+};
