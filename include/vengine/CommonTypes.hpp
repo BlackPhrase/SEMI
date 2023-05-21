3f6757e2d15086084f68565f0e00cee9cb1c4d9a
BlackPhrase <BlackPhrase@users.noreply.github.com>
2023-05-21 14:51:00 +0000

    Update the common types header

--- include/vengine/CommonTypes.hpp
+++ include/vengine/CommonTypes.hpp
@@ -13,17 +13,17 @@
 #pragma once
 
 #include <string>
+#include <string_view>
+#include <vector>
+#include <list>
 
 #include "PlatformTypes.hpp"
-
-#ifdef _WIN32
-	#define EXPORT_FUNC [[dllexport]]
-#elif __linux__
-	#define EXPORT_FUNC [[visibility("default")]]
-#else
-	#error "Unsupported compiler!"
-#endif
 
 #define C_EXPORT extern "C" EXPORT_FUNC
 
 using tString = std::string;
+
+using tStringView = std::string_view;
+
+using tStringVec = std::vector<tString>;
+using tStringList = std::list<tString>;
