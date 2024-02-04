431a04459f5c3103b1f0b0baa7c35cf21a5cd9d4
BlackPhrase <BlackPhrase@users.noreply.github.com>
2024-02-04 09:47:40 +0000

    Partially add the license headers

--- include/vengine/input/IInput.hpp
+++ include/vengine/input/IInput.hpp
@@ -1,3 +1,13 @@
+/*
+ * This file is part of V-Engine
+ *
+ * Copyright 2018-2019, 2023 BlackPhrase
+ *
+ * Licensed under terms of the MIT license
+ * See LICENSE.md file for full terms
+ * DO NOT REMOVE THIS NOTICE!
+*/
+
 /// @file
 /// @brief input module interface
 
--- src/engine/core/ListenServerMode.cpp
+++ src/engine/core/ListenServerMode.cpp
@@ -1,3 +1,13 @@
+/*
+ * This file is part of V-Engine
+ *
+ * Copyright 2017-2018, 2024 BlackPhrase
+ *
+ * Licensed under terms of the MIT license
+ * See LICENSE.md file for full terms
+ * DO NOT REMOVE THIS NOTICE!
+*/
+
 #include "ListenServerMode.hpp"
 #include "DedicatedServerMode.hpp"
 #include "DedicatedClientMode.hpp"
